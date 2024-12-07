c28991d417978dbde3b87a3060102d881a3f1d9a
Sophie Dufrane <dufranesophie@gmail.com>
2024-12-07 16:56:53 +0000

    Add generic Post view

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-sophiedufran-djangoblog-0hzrvmj9cdb.ws.codeinstitute-ide.net",".herokuapp.com"]
 
