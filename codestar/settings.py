274c69205f21cbe82f1c93ab5f7ea13ce9c3f288
Sophie Dufrane <dufranesophie@gmail.com>
2024-12-15 19:24:45 +0000

    Add testing files

--- codestar/settings.py
+++ codestar/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -27,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["8000-sophiedufran-djangoblog-0hzrvmj9cdb.ws.codeinstitute-ide.net",".herokuapp.com"]
+ALLOWED_HOSTS = ["8000-sophiedufran-djangoblog-fxib5551158.ws.codeinstitute-ide.net",".herokuapp.com"]
 
 
 # Application definition
@@ -107,6 +108,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
